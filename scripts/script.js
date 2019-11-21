06a971c910a71dcf59a9c9507ef8ef104d735a90
Daniil <daniil210910ee@gmail.com>
2019-11-21 23:11:33 +0200

    New color bulp

--- scripts/script.js
+++ scripts/script.js
@@ -2,9 +2,16 @@ function onBulp() {
     const bulp = document.getElementsByTagName("i");
     const light = document.getElementsByTagName("body");
     const text = document.getElementsByTagName("h1");
-    bulp[0].style.color = "rgb(251, 255, 0)";
+    const newColor = document.getElementById("color").value;
+    if (newColor) {
+        bulp[0].style.color = newColor;
+    } else {
+        bulp[0].style.color = "#ffed00";
+    }
     light[0].style.backgroundColor = "#ffffff";
-    text[0].innerHTML = "Bulb is: ON"
+    text[0].innerHTML = "Bulb is: ON";
+
+
 }
 
 function offBulp() {
