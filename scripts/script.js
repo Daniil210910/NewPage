71e89ba74305e239b0d91ba0936e0a4c5879c6b7
Daniil <daniil210910ee@gmail.com>
2019-11-21 21:37:39 +0200

    bulp

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,18 @@
-// alert("Hello!)");
+function onBulp() {
+    const bulp = document.getElementsByTagName("i");
+    const light = document.getElementsByTagName("body");
+    const text = document.getElementsByTagName("h1");
+    bulp[0].style.color = "rgb(251, 255, 0)";
+    light[0].style.backgroundColor = "#ffffff";
+    text[0].innerHTML = "Bulb is: ON"
+}
+
+function offBulp() {
+    const bulp = document.getElementsByTagName("i");
+    const light = document.getElementsByTagName("body");
+    const text = document.getElementsByTagName("h1");
+    bulp[0].style.color = "#000000";
+    light[0].style.backgroundColor = "rgb(160, 160, 160)";
+    text[0].innerHTML = "Bulb is: OFF"
+
+}
