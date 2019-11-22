f6b8adc05224a88486914f362f55031559c1bc3a
Daniil <daniil210910ee@gmail.com>
2019-11-22 15:10:20 +0200

    New color bulb

--- scripts/script.js
+++ scripts/script.js
@@ -1,25 +1,22 @@
-function onBulp() {
-    const bulp = document.getElementsByTagName("i");
+function onBulb() {
+    const bulb = document.getElementsByTagName("i");
     const light = document.getElementsByTagName("body");
     const text = document.getElementsByTagName("h1");
     const newColor = document.getElementById("color").value;
     if (newColor) {
-        bulp[0].style.color = newColor;
+        bulb[0].style.color = newColor;
     } else {
-        bulp[0].style.color = "#ffed00";
+        bulb[0].style.color = "#ffed00";
     }
     light[0].style.backgroundColor = "#ffffff";
     text[0].innerHTML = "Bulb is: ON";
-
-
 }
 
-function offBulp() {
-    const bulp = document.getElementsByTagName("i");
+function offBulb() {
+    const bulb = document.getElementsByTagName("i");
     const light = document.getElementsByTagName("body");
     const text = document.getElementsByTagName("h1");
-    bulp[0].style.color = "#000000";
+    bulb[0].style.color = "#000000";
     light[0].style.backgroundColor = "rgb(160, 160, 160)";
-    text[0].innerHTML = "Bulb is: OFF"
-
+    text[0].innerHTML = "Bulb is: OFF";
 }
