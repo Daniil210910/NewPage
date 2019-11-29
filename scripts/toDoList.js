6d75ec731d7d55f977b2898fa4aaefaefe28dab7
Daniil <daniil210910ee@gmail.com>
2019-11-29 20:21:49 +0200

    Problem JS

--- scripts/toDoList.js
+++ scripts/toDoList.js
@@ -8,5 +8,43 @@ let notImportantAndNotUrgent;
     importantAndNotUrgent = document.getElementById("importantAndNotUrgent");
     notImportantAndUrgent = document.getElementById("notImportantAndUrgent");
     notImportantAndNotUrgent = document.getElementById("notImportantAndNotUrgent");
-    console.log(importantAndUrgent, importantAndNotUrgent);
+
+    quadrants = [importantAndUrgent, importantAndNotUrgent, notImportantAndUrgent, notImportantAndNotUrgent];
+
+    quadrants.forEach(quadrant => {
+        const button = quadrant.getElementsByTagName("button")[0];
+        button.addEventListener("click", () => {
+            addNewTask(quadrant);
+        });
+        // console.log(button);
+    });
+
 })();
+
+function addNewTask(quadrant) {
+    const value = getInputValue(quadrant);
+    if (value) {
+        addNewItem(value, quadrant);
+
+    } else {
+        alert("Please add value to input")
+    }
+}
+
+function getInputValue(quadrant) {
+    return quadrant.getElementsByClassName("form-control")[0].value;
+
+}
+
+function addNewItem(value, quadrant) {
+    const list = quadrant.getElementsByClassName("toDoList")[0];
+    const newLi = getLiWithText(value);
+    list.appendChild(newLi);
+    console.log(list);
+}
+
+function getLiWithText(value) {
+    const newLiItem = document.createElement("li");
+    const text = document.createTextNode(value);
+    newLiItem.appendChild(text);
+}
