b0fac78745edef1f9b436686aae6a41a34167946
Daniil <daniil210910ee@gmail.com>
2019-11-29 15:24:43 +0200

    Problem JS

---
+++ scripts/toDoList.js
@@ -0,0 +1,12 @@
+let importantAndUrgent;
+let importantAndNotUrgent;
+let notImportantAndUrgent;
+let notImportantAndNotUrgent;
+
+(function () {
+    importantAndUrgent = document.getElementById("importantAndUrgent");
+    importantAndNotUrgent = document.getElementById("importantAndNotUrgent");
+    notImportantAndUrgent = document.getElementById("notImportantAndUrgent");
+    notImportantAndNotUrgent = document.getElementById("notImportantAndNotUrgent");
+    console.log(importantAndUrgent, importantAndNotUrgent);
+})();
